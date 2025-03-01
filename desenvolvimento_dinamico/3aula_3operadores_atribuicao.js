bafe609073e11271369faf9a7bb278f2c4df5341
Pedro Cattete <pedro.cattete@gmail.com>
2025-03-01 14:08:01 -0300

    Create 3aula_3operadores_atribuicao.js

---
+++ desenvolvimento_dinamico/3aula_3operadores_atribuicao.js
@@ -0,0 +1,36 @@
+// Operador de Atribuição
+/*
+Exemplos:
++=
+-=
+*=
+/=
+*/
+
+// Valor de Atribuição básico:
+let x = 10;
+console.log(`Valor inicial de x: ${x}`);
+
+// Operador de Atribuição de Adição
+x += 5; //Equivalente x = x + 5
+console.log(`Após x += 5, valor de x: ${x}`)
+
+// Operador de Atribuição de Subtração
+x -= 3; //Equivalente x = x - 3
+console.log(`Após x -= 3, valor de x: ${x}`)
+
+// Operador de Atribuição de Multiplicação
+x *= 2; //Equivalente x = x * 2
+console.log(`Após x *= 2, valor de x: ${x}`)
+
+// Operador de Atribuição de Divisão
+x /= 4; //Equivalente x = x / 4
+console.log(`Após x /= 4, valor de x: ${x}`)
+
+// Operador de Atribuição de Módulo/Resto
+x %= 4; //Equivalente x = x % 4
+console.log(`Após x %= 4, valor de x: ${x}`)
+
+// Operador de Atribuição de Exponecial
+x **= 3; //Equivalente x = x ** 3
+console.log(`Após x **= 3, valor de x: ${x}`)
