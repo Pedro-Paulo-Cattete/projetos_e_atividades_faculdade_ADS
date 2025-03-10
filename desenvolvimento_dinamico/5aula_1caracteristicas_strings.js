c7d13e3ad5afef8073dd25ffea19e102a0f4878c
Pedro Cattete <pedro.cattete@gmail.com>
2025-03-10 09:53:47 -0300

    Create 5aula_1caracteristicas_strings.js

---
+++ desenvolvimento_dinamico/5aula_1caracteristicas_strings.js
@@ -0,0 +1,15 @@
+// Tipo de Dados String
+// Uma String é um tipo de dado que representa uma sequência de caracteres
+
+// Declaração:
+// Strings são declaradas por usando aspas ('') ou aspas duplas ("")
+// Sempre manter um padrão de declaração para um código
+
+// Concatenação:
+// O ato de juntar duas strings (Podem ser concatenadas usando o operador de '+')
+
+// Comprimento:
+// A propriedade 'lenght' pode ser usada para obter o comprimento de uma string.
+
+// Métodos
+// Funções especifícas para manipular strings de diversas formas
