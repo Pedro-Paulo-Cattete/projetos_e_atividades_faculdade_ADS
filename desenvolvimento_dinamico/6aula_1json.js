2396860ce93db45a530f2888520a4aa50d37d4ff
Pedro Cattete <pedro.cattete@gmail.com>
2025-03-15 14:34:24 -0300

    Create 6aula_1json.js

---
+++ desenvolvimento_dinamico/6aula_1json.js
@@ -0,0 +1,22 @@
+//JSON:
+/*
+JavaScript Object Notation - É um formato de dados de fácil leitura usado para trocar e armazenar dados.
+Base: Sintaxe de objeto em JavaScript, mas é independente de linguagem, o que significa que pode ser usando em praticamente qualquer linguagem de programação.
+
+// Utilização:
+/*
+A utilização é muito comum e uma variedade de cenários de desenvolvimento de software, especialmente em aplicativos web e serviços web.
+- Comunicação entre cliente e servidor
+- Armazenamento de dados
+- Integração de sistemas
+- Consumo de APIs
+*/
+
+// Visualização de Objetos:
+/*
+Exemplos de ambientes de visualização:
+Console do navegador
+Formatação em linha
+Formatação com estilo
+Ferramentas online - Exemplo: JSON Formatter
+*/
