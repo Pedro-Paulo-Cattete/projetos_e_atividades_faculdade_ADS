96b21232c707fcc26d50cfd8262d8efa3fad07de
Pedro Cattete <pedro.cattete@gmail.com>
2025-03-22 10:04:31 -0300

    Create 7aula_2array_metodos.js

---
+++ desenvolvimento_dinamico/7aula_2array_metodos.js
@@ -0,0 +1,15 @@
+/*
+Métodos básicos de Array:
+push() / pop() / shift()
+
+push: "Empurrar" elementos ao array, irá adicionar na próximma posição no array
+pop: "Retira" elementos do array, expele elementos da última posição para a primeira
+shift: "Remove a esquerda" elementos do array, expele elementos da primeira posição do array
+*/
+
+// Métodos básicos:
+
+let frutas = ["maçã", "banana", "laranja", "uva"];
+
+console.log("Array inicial: " + frutas);
+console.log("Comprimento do array: " + frutas.length);
