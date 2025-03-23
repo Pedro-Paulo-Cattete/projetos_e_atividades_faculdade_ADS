545853cdf8a7cdfe3e9d03f333169c81d382c802
Pedro Cattete <pedro.cattete@gmail.com>
2025-03-23 18:55:07 -0300

    Create 8aula_1objetos_conceitos_de_orientacao.js

---
+++ desenvolvimento_dinamico/8aula_1objetos_conceitos_de_orientacao.js
@@ -0,0 +1,7 @@
+/*
+Conceito de Orientação a Objetos:
+É uma forma de estruturar e projetar o mundo real no código,
+se baseando no conceito de objetos que interagem entre si.
+Principal Objetivo: Organizar e estuturar o código de forma mais modular, flexível e reutilizável
+*/
+
