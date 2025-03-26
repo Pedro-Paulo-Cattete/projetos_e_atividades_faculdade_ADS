8f516311245f7095880becc26d86d28f4cb73cec
Pedro Cattete <pedro.cattete@gmail.com>
2025-03-26 08:05:56 -0300

    Update 8aula_1objetos_conceitos_de_orientacao.js

--- desenvolvimento_dinamico/8aula_1objetos_conceitos_de_orientacao.js
+++ desenvolvimento_dinamico/8aula_1objetos_conceitos_de_orientacao.js
@@ -5,3 +5,30 @@ se baseando no conceito de objetos que interagem entre si.
 Principal Objetivo: Organizar e estuturar o código de forma mais modular, flexível e reutilizável
 */
 
+/*
+Definição de Classes e Objetos:
+Classe: Uma estrutura que define um tipo de objeto
+Objeto: Uma instância de uma classe específica
+*/
+
+// Exemplo:
+
+// Classe: Casa
+
+class casa {
+
+    // Construtor define as propriedades da classe
+    constructor (cor, numQuarto, temGaragem) {
+        this.cor = cor;
+    }
+
+
+    // Método para descrever a casa
+    descrever () {
+        let descricao = "";
+
+        return descricao;
+    }
+
+
+}
